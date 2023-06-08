7c2ad569d59d8338003a01c40f604f35e5546f67
Thomas <losromeu@hotmail.com>
2023-06-08 13:26:30 -0300

    escondemos el teclado despues de buscar

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.retrofit
 
 import android.annotation.SuppressLint
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.autofill.FillEventHistory
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.SearchView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,27 +17,30 @@ import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: BreedsAdapter
     private var imagesByBreedList = mutableListOf<String>()
+    private lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         recyclerView = findViewById(R.id.recycler)
+        searchView = findViewById(R.id.searchview)
+
+        searchView.setOnQueryTextListener(this)
+
         recyclerView.layoutManager = LinearLayoutManager(this)
         adapter = BreedsAdapter(imagesByBreedList)
         recyclerView.adapter = adapter
-
-        getImageByBreedList()
     }
 
     @SuppressLint("NotifyDataSetChanged")
-    private fun getImageByBreedList() {
+    private fun getImageBy(breed: String?) {
 
         CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit().create(ApiService::class.java).getImagesByBreeds("breed/hound/images")
+            val call = getRetrofit().create(ApiService::class.java).getImagesByBreeds("breed/$breed/images")
             val response = call.body()
 
             runOnUiThread {
@@ -44,8 +52,18 @@ class MainActivity : AppCompatActivity() {
                 }else{
                     showError()
                 }
+                hideKeyBoard()
             }
         }
+    }
+
+    private fun hideKeyBoard() {
+        val imm = this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        var view: View? = this.currentFocus
+        if (view == null){
+            view = View(this);
+        }
+        imm.hideSoftInputFromWindow(view.windowToken, 0);
     }
 
     private fun showError(){
@@ -56,5 +74,16 @@ class MainActivity : AppCompatActivity() {
             .baseUrl("https://dog.ceo/api/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
+    override fun onQueryTextSubmit(breed: String?): Boolean {
+        if (!breed.isNullOrBlank()) {
+            getImageBy(breed)
+        }
+        return true
+    }
+
+    override fun onQueryTextChange(p0: String?): Boolean {
+        return false
     }
 }
