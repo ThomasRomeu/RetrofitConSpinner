df35d5902b0c57829f257da59a7f510350e3e3a8
Thomas <losromeu@hotmail.com>
2023-06-08 12:26:19 -0300

    Hacemos el llamado a la API

--- app/src/main/java/com/example/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/retrofit/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.retrofit
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: BreedsAdapter
+    private var imagesByBreedList = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        recyclerView = findViewById(R.id.recycler)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = BreedsAdapter(imagesByBreedList)
+        recyclerView.adapter = adapter
+
+        getImageByBreedList()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private fun getImageByBreedList() {
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val call = getRetrofit().create(ApiService::class.java).getImagesByBreeds("breed/hound/images")
+            val response = call.body()
+
+            runOnUiThread {
+                if(call.isSuccessful){
+                    val images = response?.images ?: emptyList()
+                    imagesByBreedList.clear()
+                    imagesByBreedList.addAll(images)
+                    adapter.notifyDataSetChanged()
+                }else{
+                    showError()
+                }
+            }
+        }
+    }
+
+    private fun showError(){
+        Toast.makeText(this, "Fallo en la llamada", Toast.LENGTH_SHORT).show()
+    }
+    private fun getRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl("https://dog.ceo/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
     }
 }
